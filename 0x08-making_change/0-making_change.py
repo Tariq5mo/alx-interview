d262dc464877abbd313c5814bb1fd726eee7b583
Tariq5mo <tariqomer756@gmail.com>
2024-11-28 11:30:58 +0200

    Fix task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -20,13 +20,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    database = [float('inf')] * (total + 1)
-    database[0] = 0
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total == 0:
+            break
+        if coin <= total:
+            count += total // coin
+            total %= coin
 
-    for quantity in range(1, total + 1):
-        for coin in coins:
-            if coin <= quantity:
-                database[quantity] = min(database[quantity],
-                                         database[quantity - coin] + 1)
-
-    return database[total] if database[total] != float('inf') else -1
+    return count if total == 0 else -1
